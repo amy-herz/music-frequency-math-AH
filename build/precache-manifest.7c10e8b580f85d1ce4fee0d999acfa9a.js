3295ac7e7e65004fe345eb7a7f9ede3f2ccd0d09
Amy Herz <amyt.herz@gmail.com>
2020-06-12 11:24:51 -0500

    added Procfile

---
+++ build/precache-manifest.7c10e8b580f85d1ce4fee0d999acfa9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71cbb3c922a065a14d84bcad7fa6816",
+    "url": "/index.html"
+  },
+  {
+    "revision": "490140c90df91ec156a5",
+    "url": "/static/css/2.7cc36aad.chunk.css"
+  },
+  {
+    "revision": "63d884e00b0d46b9a9a3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "490140c90df91ec156a5",
+    "url": "/static/js/2.2a2aa2d2.chunk.js"
+  },
+  {
+    "revision": "b28590a4d5a036be9947256ff9f1f35b",
+    "url": "/static/js/2.2a2aa2d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63d884e00b0d46b9a9a3",
+    "url": "/static/js/main.daff2015.chunk.js"
+  },
+  {
+    "revision": "85ee8ce020f61fd557c6",
+    "url": "/static/js/runtime-main.6d1c7e0c.js"
+  }
+]);
